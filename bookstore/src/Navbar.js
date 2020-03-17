7901303003682041a671ea330e7f054aa17afbbe
Humam Abdullah <humam.abdullah@naicoits.com>
2020-03-17 12:22:13 +0530

    Initial UI

---
+++ bookstore/src/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
