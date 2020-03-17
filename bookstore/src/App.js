60a54d48f5fb78c7583c956d4b063dbcf4094004
Humam Abdullah <humam.abdullah@naicoits.com>
2020-03-17 12:45:17 +0530

    Navbar,Card

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import BookCard from "./Card";
+import Navbar from "./Navbar";
 
 class App extends React.Component {
   render() {
     return (
       <div>
+      <Navbar></Navbar>
       <BookCard></BookCard>
       </div>
     );
--- bookstore/src/Card.js
+++ bookstore/src/Card.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./App.css";
 
@@ -6,17 +7,12 @@ class BookCard extends React.Component {
     return (
       <div class="card card_style">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="/" class="card-link">
-            Card link
-          </a>
-          <a href="/" class="card-link">
-            Another link
+          <h5 class="card-title">Bookname</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Category</h6>
+          <h6>Author</h6>
+          <h6>Cost</h6>
+          <a href="#" class="card-link">
+          <button class="btn btn-outline-primary">Buy</button>
           </a>
         </div>
       </div>
