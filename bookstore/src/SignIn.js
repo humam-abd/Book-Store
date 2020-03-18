32c965e87020594d167fe21a4ef69e604ca662f2
Humam Abdullah <humam.abdullah@naicoits.com>
2020-03-18 18:38:41 +0530

    Session, Addbook states

--- bookstore/src/SignIn.js
+++ bookstore/src/SignIn.js
@@ -1,11 +1,47 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
+import { baseurl } from "./baseurl";
 
 class Signin extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
   signIn = event => {
     event.preventDefault();
-    localStorage.setItem("session", 1);
-    this.props.history.push("/main");
+    var uname = this.state.username;
+    var pass = this.state.password;
+    axios.post(`${baseurl}Admin?username=${uname}&password=${pass}`).then(
+      result => {
+        if (result.data != "") {
+          console.log(result);
+          if (
+            result.data[0].Username == uname &&
+            result.data[0].Password == pass
+          ) {
+            localStorage.setItem("session", 1);
+            this.props.history.push("/main");
+          } else {
+            alert("Not a valid user");
+          }
+        } else {
+          alert("Not a valid user");
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  };
+  userName = event => {
+    this.setState({ username: event.target.value });
+  };
+  passWord = event => {
+    this.setState({ password: event.target.value });
   };
   render() {
     return (
@@ -16,6 +52,8 @@ class Signin extends React.Component {
               type="text"
               className="form-control inp"
               placeholder="Username"
+              value={this.state.username}
+              onChange={this.userName}
             />
           </div>
           <div className="form-group">
@@ -23,6 +61,8 @@ class Signin extends React.Component {
               type="password"
               className="form-control inp"
               placeholder="Password"
+              value={this.state.password}
+              onChange={this.passWord}
             />
           </div>
           <button type="submit" className="btn btn-primary">
